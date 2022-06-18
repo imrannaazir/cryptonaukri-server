1d01e7a5fe160d29e05ff5ec77b50eca6a389e1b
unknown <imran nazir>
2022-06-18 15:36:15 +0600

    created post api for applicants

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ app.use(cors())
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.geuva.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const applicantCollection = client.db("crypto").collection("applicants");
+
+
+        //post api for applicants
+        app.post('/applicants', async (req, res) => {
+            const newApplicant = req.body;
+            const result = await applicantCollection.insertOne(newApplicant);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 //root api
 app.get("/", (req, res) => {
     res.send("Hello browser!")
